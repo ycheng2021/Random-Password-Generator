c50f5a1eb25fc6014dbcf5fd2c17f68369422e67
Yang Cheng <annacheng09@hotmail.com>
2022-02-25 15:43:09 -0800

    add character arrays, input between 8=128

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,22 @@
 let generatePassword = function() {
 
     // set 4 arrays
-
+    const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+    
     // empty array to add into for condition
+    const database = [];
 
     // prompts the question on password length
+    let userInput = prompt("Please choose a length between 8 to 128 characters");
 
     // user has to input number between 8 - 128
+    if (userInput < 8 || userInput > 128) {
+        alert("Error! Please choose a number between 8 and 128")
+        userInput = prompt("Please choose a length between 8 to 128 characters")
+    }
 
     // ask if user wants lowercase
 
