bc0abeef8fbb27042ad116fa68f7795ab630621e
Yang Cheng <annacheng09@hotmail.com>
2022-02-28 15:11:14 -0800

    edit global/local scope, address if no type selected

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Random-Password-Generator
-This webpage will allow the user to generate a random, secure password that contains at least 8 characters and no more than 128 characters. When prompted, the user will be asked questions to decide whether or not the password should contain lowercase, uppercase, numeric, and/or special characters. 
+This webpage will allow the user to generate a random, secure password that contains at least 8 characters and no more than 128 characters. When prompted, the user will be asked questions to decide whether or not the password should contain lowercase, uppercase, numeric, and/or special characters. User must choose at least one character type to generate a password. 
+
+## Links 
+
+https://github.com/ycheng2021/Random-Password-Generator
+
+https://ycheng2021.github.io/Random-Password-Generator/
+
+![screenshot](./assets/images/Random-Password-Generator_screenshot.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,74 @@
 // My code here
+// set 4 arrays
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+
+// empty array to add into for condition
+const database = [];
+
+let userInput;
+
+function questions() {
+     // prompts the question on password length
+     userInput = prompt("Please choose a length between 8 to 128 characters");
+
+     // user has to input number between 8 - 128
+     if (userInput < 8 || userInput > 128) {
+         alert("Error! Please choose a number between 8 and 128")
+         userInput = prompt("Please choose a length between 8 to 128 characters")
+     }
+ 
+     // ask if user wants lowercase
+     let yesLower = window.confirm("Would you like to include lowercase letters?");
+     if (yesLower) {
+         database.push(...lowercase)
+     }
+     // ask if user wants uppercase
+     let yesUpper = window.confirm("Would you like to include uppercase letters?");
+     if (yesUpper) {
+         database.push(...uppercase)
+     }
+     // ask if user wants numbers
+     let yesNum = window.confirm("Would you like to include numbers?");
+     if (yesNum) {
+         database.push(...numbers)
+     }
+     // ask if user wants special characters
+     let yesSpecial = window.confirm("Would you like to include special characters?");
+     if (yesSpecial) {
+         database.push(...specials)
+     }
+ 
+     if (!yesLower && !yesUpper && !yesNum && !yesSpecial) {
+         alert("Error! Please choose at least one character type.")
+         return false;
+     } 
+}
 
 let generatePassword = function() {
 
-    // set 4 arrays
-    const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    const specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
-    
-    // empty array to add into for condition
-    const database = [];
+    var conditions = questions();
 
-    // prompts the question on password length
-    let userInput = prompt("Please choose a length between 8 to 128 characters");
-
-    // user has to input number between 8 - 128
-    if (userInput < 8 || userInput > 128) {
-        alert("Error! Please choose a number between 8 and 128")
-        userInput = prompt("Please choose a length between 8 to 128 characters")
-    }
-
-    // ask if user wants lowercase
-    let yesLower = window.confirm("Would you like to include lowercase letters?");
-    if (yesLower) {
-        database.push(...lowercase)
-    }
-    // ask if user wants uppercase
-    let yesUpper = window.confirm("Would you like to include uppercase letters?");
-    if (yesUpper) {
-        database.push(...uppercase)
-    }
-    // ask if user wants numbers
-    let yesNum = window.confirm("Would you like to include numbers?");
-    if (yesNum) {
-        database.push(...numbers)
-    }
-    // ask if user wants special characters
-    let yesSpecial = window.confirm("Would you like to include special characters?");
-    if (yesSpecial) {
-        database.push(...specials)
+    if (conditions === false) {
+        questions();
     }
 
     // set variable to use for index for randomizing
     // set up another array to hold generated random characters
     let genCharacters = [];
     // for loop that will take all above info and generate a string
-    console.log(userInput)
     for (let i=0; i< userInput; i++) {
         let randomNum = Math.floor(Math.random() * database.length-1)
         genCharacters.push(database[randomNum]);
     }
-    console.log(genCharacters[0]);
     // join array and return password
     let outputPassword = genCharacters.join('');
 
     return outputPassword;
     
 } 
-
-generatePassword();
 
 // Starting code
 var generateBtn = document.querySelector("#generate");
